4b33012587c0dc1d9cee2c3f7f2cff07fa3d571a
waspnesser <fuzuli_munir@protonmail.com>
2020-04-10 23:31:08 +0300

    setup.py Hashing was forgatten. Solved.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from encryption import encrypt
 
+from passlib.hash import sha256_crypt
+
 def setup(filePath,password):
 	with open(filePath,"r") as file:
 		data = file.read()
 	
 	data = data.encode("utf-8")
 	password = password.encode("utf-8")
+	hashed = sha256_crypt.hash(password)
 	encrypted = encrypt(password,data)
 	with open("encrypted.txt","w") as file:
 		file.write(encrypted)
-
+	
+	with open("hashed.txt","w") as file:
+		file.write(hashed)
+		
 if __name__ == "__main__":
 	filePath = input("File Path : ")
 	password = input("Password : ")
